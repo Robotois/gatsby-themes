5288e9afd00383a66fe754c0e45c49b18a6f1ae5
manduks <iam@armando.mx>
2019-10-02 22:58:09 -0500

    move theme switcher to avoid overlapping of facebook messenger

--- gatsby-robotois-academy/src/components/theme-switcher.js
+++ gatsby-robotois-academy/src/components/theme-switcher.js
@@ -10,7 +10,7 @@ export default function ThemeSwitcher() {
     <div style={{
       position: 'fixed',
       bottom: 10,
-      right: 20,
+      left: 20,
     }}>
       <Switch
         onChange={value => {
